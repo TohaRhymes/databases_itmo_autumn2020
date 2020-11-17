c6b06b4a51848d529c0042b62e7f91bf073633a7
Anton Changalidi <anton.chana@gmail.com>
2020-11-18 00:19:41 +0300

    3rd lab with good plans + start of 3rd stage of project

--- project/creation.sql
+++ project/creation.sql
@@ -1,62 +1,73 @@
+CREATE TYPE pathogen_type AS ENUM ('virus', 'bacterium', 'protozoan', 'prion', 'viroid', 'fungus', 'small animal');
+CREATE TYPE drugs_groups AS ENUM ('Group A (prohibited substances)', 'Group B (limited turnover)', 'Group C (free circulation)');
+CREATE TYPE poison_origin AS ENUM ('nature', 'chemicals');
+CREATE TYPE development_stage AS ENUM ('Preclinical phase', 'Phase 0', 'Phase I', 'Phase II', 'Phase III', 'Phase IV');
+CREATE TYPE stock_availability AS ENUM ('available', 'in other shops', 'ended');
+
+
 CREATE TABLE pathogens
 (
-    id         SERIAL PRIMARY KEY,
-    name       VARCHAR(80) NOT NULL,
-    type       VARCHAR(80) NOT NULL,
-    action     VARCHAR(80) NOT NULL,
-    antibiotic VARCHAR(80) NOT NULL
+    id     SERIAL PRIMARY KEY,
+    name   VARCHAR(80)   NOT NULL,
+    type   pathogen_type NOT NULL,
+    action VARCHAR(80)   NOT NULL
 );
 CREATE TABLE diseases
 (
     id          SERIAL PRIMARY KEY,
-    pathogen_id SERIAL
-        CONSTRAINT fk_pathogens_id REFERENCES pathogens (id),
+    pathogen_id INTEGER
+        CONSTRAINT fk_pathogens_id REFERENCES pathogens (id) ON DELETE CASCADE,
     mortality   DECIMAL NOT NULL
         DEFAULT 0 CHECK ( mortality >= 0 and mortality <= 1 )
 );
 CREATE TABLE companies
 (
-    id                    SERIAL PRIMARY KEY,
-    name                  VARCHAR(80) UNIQUE NOT NULL,
-    specialization        VARCHAR(80)        NOT NULL,
-    share_stock_existence BOOLEAN            NOT NULL
+    id                           SERIAL PRIMARY KEY,
+    name                         VARCHAR(80) UNIQUE NOT NULL,
+    specialization               VARCHAR(80)        NOT NULL,
+    share_stock_existence        BOOLEAN            NOT NULL,
+    market_cap                   DECIMAL CHECK ( market_cap >= 0 ),
+    net_profit_margin_pct_annual DECIMAL CHECK ( net_profit_margin_pct_annual >= 0 ) -- ЧЕ это?))))
 );
 CREATE TABLE company_info
 (
     id                           SERIAL PRIMARY KEY,
-    company_id                   SERIAL
-        CONSTRAINT fk_companies_id REFERENCES companies (id),
-    market_cap                   DECIMAL,
-    net_profit_margin_pct_annual DECIMAL
+    company_id                   INTEGER
+        CONSTRAINT fk_companies_id REFERENCES companies (id) ON DELETE CASCADE
+        UNIQUE NOT NULL,
+    restore_date                 TIMESTAMP,
+    market_cap                   DECIMAL CHECK ( market_cap >= 0 ),
+    net_profit_margin_pct_annual DECIMAL CHECK ( net_profit_margin_pct_annual >= 0 )
 );
 CREATE TABLE drugs
 (
     id               SERIAL PRIMARY KEY,
     active_substance VARCHAR(80) UNIQUE NOT NULL,
-    homeopathy       BOOLEAN            NOT NULL,
-    drugs_group      VARCHAR(80)        NOT NULL
+    homeopathy       BOOLEAN            NOT NULL
+        DEFAULT false,
+    drugs_group      drugs_groups       NOT NULL
 );
 CREATE TABLE poisons
 (
-    id                    SERIAL PRIMARY KEY,
-    active_substance      VARCHAR(80) UNIQUE NOT NULL,
-    poison_type_by_action VARCHAR(80)        NOT NULL,
-    poison_type_by_origin VARCHAR(80)        NOT NULL,
-    mortality             DECIMAL            NOT NULL
+    id               SERIAL PRIMARY KEY,
+    active_substance VARCHAR(80) UNIQUE NOT NULL,
+    type_by_action   VARCHAR(80)        NOT NULL,
+    type_by_origin   poison_origin,
+    mortality        DECIMAL            NOT NULL
         DEFAULT 0 CHECK ( mortality >= 0 and mortality <= 1 )
 );
 
 CREATE TABLE development
 (
     id            SERIAL PRIMARY KEY,
-    company_name  VARCHAR(80)
-        CONSTRAINT fk_companies_name
-            REFERENCES companies (name) NOT NULL,
-    pathogen_id   SERIAL
+    company_id    INTEGER
+        CONSTRAINT fk_companies_id
+            REFERENCES companies (id) ON DELETE CASCADE NOT NULL,
+    pathogen_id   INTEGER
         CONSTRAINT fk_pathogens_id
-            REFERENCES pathogens (id)   NOT NULL,
-    testing_stage VARCHAR(80)           NOT NULL,
-    success       VARCHAR(80)           NOT NULL
+            REFERENCES pathogens (id) ON DELETE CASCADE NOT NULL,
+    testing_stage development_stage                     NOT NULL,
+    failed        BOOLEAN                               NOT NULL DEFAULT false
 );
 CREATE TABLE patents
 (
@@ -65,33 +76,42 @@ CREATE TABLE patents
 );
 CREATE TABLE trademarks
 (
-    id               SERIAL PRIMARY KEY,
-    name             VARCHAR(80) UNIQUE           NOT NULL,
-    active_substance VARCHAR(80)
-        CONSTRAINT fk_drugs_active_substance
-            REFERENCES drugs (active_substance),
-    company_name     VARCHAR(80)
-        CONSTRAINT fk_companies_name
-            REFERENCES companies (name)           NOT NULL,
-    doze             DECIMAL                      NOT NULL,
-    avg_price        DECIMAL                      NOT NULL,
-    patent_id        SERIAL
+    id            SERIAL PRIMARY KEY,
+    name          VARCHAR(80) UNIQUE NOT NULL,
+    doze          DECIMAL            NOT NULL CHECK ( doze > 0 ),
+    release_price DECIMAL            NOT NULL check ( release_price > 0 ),
+    drug_id       INTEGER
+        CONSTRAINT fk_drugs_id
+            REFERENCES drugs (id) ON DELETE CASCADE,
+    company_id    INTEGER
+        CONSTRAINT fk_companies_id
+            REFERENCES companies (id) ON DELETE CASCADE
+                                     NOT NULL,
+    patent_id     INTEGER
         CONSTRAINT fk_patents_patent_id
-            REFERENCES patents (patent_id) UNIQUE NOT NULL
+            REFERENCES patents (patent_id) ON DELETE CASCADE
+        UNIQUE
 );
 CREATE TABLE pharmacies
 (
-    id   SERIAL PRIMARY KEY,
-    name VARCHAR(80) NOT NULL
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR(80) NOT NULL,
+    price_mul  DECIMAL     NOT NULL CHECK ( price_mul >= 1 ),
+    price_plus DECIMAL     NOT NULL CHECK ( price_plus >= 0 )
 );
+
 CREATE TABLE stock
 (
     id           SERIAL PRIMARY KEY,
-    pharmacy_id  SERIAL
-        CONSTRAINT fk_pharmacy_id REFERENCES pharmacies (id),
-    trademark_id SERIAL
-        CONSTRAINT fk_trademarks_id REFERENCES trademarks (id),
-    availability VARCHAR(80) --in stock, pharmacy_id, null
+    pharmacy_id  INTEGER
+        CONSTRAINT fk_pharmacy_id REFERENCES pharmacies (id) ON DELETE CASCADE
+                                   NOT NULL,
+    trademark_id INTEGER
+        CONSTRAINT fk_trademarks_id REFERENCES trademarks (id) ON DELETE CASCADE
+                                   NOT NULL,
+    availability stock_availability,
+    amount       INTEGER default 0 NOT NULL CHECK ( amount >= 0 ),
+    price        DECIMAL           NOT NULL check ( price > 0 )
 );
 CREATE TABLE ethnoscience
 (
@@ -101,22 +121,28 @@ CREATE TABLE ethnoscience
 );
 CREATE TABLE drugs_to_diseases
 (
-    drugs_id   SERIAL
-        CONSTRAINT fk_drugs_id REFERENCES drugs (id),
-    disease_id SERIAL
-        CONSTRAINT fk_diseases_id REFERENCES diseases (id)
+    drugs_id   INTEGER
+        CONSTRAINT fk_drugs_id REFERENCES drugs (id) ON DELETE CASCADE
+        NOT NULL,
+    disease_id INTEGER
+        CONSTRAINT fk_diseases_id REFERENCES diseases (id) ON DELETE CASCADE
+        NOT NULL
 );
 CREATE TABLE drugs_to_poisons
 (
-    drugs_id  SERIAL
-        CONSTRAINT fk_drugs_id REFERENCES drugs (id),
-    poison_id SERIAL
-        CONSTRAINT fk_poisons_id REFERENCES poisons (id)
+    drugs_id  INTEGER
+        CONSTRAINT fk_drugs_id REFERENCES drugs (id) ON DELETE CASCADE
+        NOT NULL,
+    poison_id INTEGER
+        CONSTRAINT fk_poisons_id REFERENCES poisons (id) ON DELETE CASCADE
+        NOT NULL
 );
 CREATE TABLE ethnoscience_to_diseases
 (
-    ethnoscience_id SERIAL
-        CONSTRAINT fk_ethnoscience_id REFERENCES ethnoscience (id),
-    disease_id      SERIAL
-        CONSTRAINT fk_diseases_id REFERENCES diseases (id)
+    ethnoscience_id INTEGER
+        CONSTRAINT fk_ethnoscience_id REFERENCES ethnoscience (id) ON DELETE CASCADE
+        NOT NULL,
+    disease_id      INTEGER
+        CONSTRAINT fk_diseases_id REFERENCES diseases (id) ON DELETE CASCADE
+        NOT NULL
 );
